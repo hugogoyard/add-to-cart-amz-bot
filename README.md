d0f0be6f24635b9743b7633130fcfbbd00c29d73
Hugo GOYARD <goyardh@gmail.com>
2021-12-02 17:22:23 +0100

    add a README and update the script to support amazon new version

---
+++ README.md
@@ -0,0 +1,23 @@
+## Getting started
+Before running this python script, make sure to have Python on your system.
+Then you will have to install the different libraries (if you don't already have them) that are in this script.
+
+```shell
+pip install selenium
+```
+```shell
+pip install colorama
+```
+
+Then you will have to download the lastest ChromeDriver that you can found with this URL:
+https://sites.google.com/chromium.org/driver/
+
+
+It will download an archive, you will have to take the file in this archive and copy it in the project folder.
+When its done, you have finish the getting started step and you can now execute the script
+
+## Start the script
+To start the Python script you can use this command
+````shell
+python script.py
+````
--- script.py
+++ script.py
@@ -9,7 +9,6 @@ import time
 init(autoreset=True)
 
 url = 'https://www.amazon.fr/PlayStation-Digital-Manette-DualSense-Couleur/dp/B08H98GVK8/ref=sr_1_1?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=ps5+digital&qid=1633869712&qsid=260-3594671-7176730&s=videogames&sr=1-1&sres=B08H98GVK8%2CB094J12PPX%2CB094VKN192%2CB08PTXSKHS%2CB091D8MDF3%2CB097XXCVKC%2CB0924452L1%2CB093QZ7YD3%2CB094DH53S7%2CB091CKVF4S%2CB09F2HMG7P%2CB091CLJY3V%2CB099HZHR84%2CB0953MWP8L%2CB098JQLW48%2CB09248RKD6'
-url = 'https://www.amazon.fr/PlayStation-officielle-DualSense-rechargeable-Compatible/dp/B08H99BPJN?ref_=ast_sto_dp&th=1&psc=1'
 res = False
 essai = 0
 
@@ -34,7 +33,7 @@ while(res == False):
         print('New Try (' + Fore.BLUE + 'trial: #' + str(essai) + Fore.RESET+')')
         browser.get(url)
     try:
-        productPrice = browser.find_element_by_id('priceblock_ourprice')
+        productPrice = browser.find_element_by_css_selector('#centerCol #apex_desktop span.a-price')
         res = True
         try:
             addToCartBtn = browser.find_element_by_id('add-to-cart-button')
@@ -59,5 +58,6 @@ else:
     time.sleep(5)
     closeRightPanel = browser.find_element_by_id('attach-popover-lgtbox').click()
     time.sleep(5)
+    browser.get(url)
     goToCartBtn = browser.find_element_by_id('nav-cart').click()
     print(Fore.GREEN + "✔ " + Fore.RESET + 'Product successfully added to your cart.')
